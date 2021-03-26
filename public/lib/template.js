5a048e8f2b15ffeeaee0c490ae92912a11754d8f
jaewoneee <jaewon810@naver.com>
2021-03-26 15:36:21 +0900

    Style 수정

--- public/lib/template.js
+++ public/lib/template.js
@@ -18,18 +18,20 @@ export default {
     },
     detail(goalTitle, currentDay, leftDay, goalCalendar, goalID) {
         return `<form action="/goal/check_goal" method="get" class="goal_detail_form">
-                    <div class="goal_top">
-                        <h2>${goalTitle}</h2>
-                        <button class="close_btn" type="button" aria-label="닫기 버튼"></button>
+                    <div class="detail_inner">
+                        <div class="goal_top">
+                            <h2>${goalTitle}</h2>
+                            <button class="close_btn" type="button" aria-label="닫기 버튼"></button>
+                        </div>
+                        <input type="hidden" name="goal_id" value=${goalID}>
+                        <div class="goal-days">
+                            <div class="current-day">${currentDay}</div>
+                            <div class="left-day">${leftDay}</div>
+                        </div>
+                        ${goalCalendar}
+                        <input type="submit" id="save_btn" aria-label="내용 저장 버튼" value="save">
+                        <input type="submit" id="delete_btn" formaction="/goal/delete_goal" aria-label="목표삭제 버튼" value="delete the goal">
                     </div>
-                    <input type="hidden" name="goal_id" value=${goalID}>
-                    <div class="goal-days">
-                        <div class="current-day">${currentDay}</div>
-                        <div class="left-day">${leftDay}</div>
-                    </div>
-                    ${goalCalendar}
-                    <input type="submit" id="save_btn" aria-label="내용 저장 버튼" value="save">
-                    <input type="submit" id="delete_btn" formaction="/goal/delete_goal" aria-label="목표삭제 버튼" value="delete the goal">
                 </form>`;
     }
 
